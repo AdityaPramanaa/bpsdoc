55c1e04d915df13c67533b749c186c8cf0227ced
taksunee <projectpramm@gmail,com>
2025-07-12 09:22:41 +0800

    fix: rename list-cloudinary.js to .cjs for Netlify CommonJS support

--- netlify/functions/delete-cloudinary.js
+++ netlify/functions/delete-cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'dqwnckoeu',
-  api_key: '345733924435416',
-  api_secret: 'p_QqW6bmYSxSADSfK7O-OCctL4k'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 exports.handler = async function(event, context) {
